1769940b1e5bc21a2b65623a493ab50aed8707a4
Rich-Nardone <64598006+Rich-Nardone@users.noreply.github.com>
2022-02-15 16:39:50 -0500

    Update PolarData.py added comments and formatted file

--- PolarData.py
+++ PolarData.py
@@ -1,13 +1,14 @@
-from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-import time
+from selenium import webdriver
+from dotenv import load_dotenv   
+import pandas as pd
 import datetime
+import time
 import glob
 import os
-import pandas as pd
-from dotenv import load_dotenv   #for python-dotenv method
+
 load_dotenv()      
 
 now = datetime.datetime.now()
@@ -15,10 +16,14 @@ year = '{:04d}'.format(now.year)
 month = '{:02d}'.format(now.month)
 day = '{:02d}'.format(now.day)
 
-POLAR_PATH =  os.environ.get('POLAR_PATH')
-POLAR_EMAIL = os.environ.get('POLAR_EMAIL')
-POLAR_PASSWORD = os.environ.get('POLAR_PASSWORD')
-DOWNLOADS_PATH = os.environ.get('DOWNLOADS_PATH')
+
+POLAR_EMAIL = os.environ.get('POLAR_EMAIL') # pull polar email
+POLAR_PASSWORD = os.environ.get('POLAR_PASSWORD') # pull polar password
+POLAR_PATH =  os.environ.get('POLAR_PATH') # pull path to polar  
+DOWNLOADS_PATH = os.environ.get('DOWNLOADS_PATH') # pull path to Downloads
+
+global SESSIONS = 0
+files = {}
 
 def CheckForSetup():
     return os.path.exists('.env')
@@ -28,9 +33,6 @@ def CreateData(user,passw,dpath,ppath):
     f.write('POLAR_EMAIL='+user+'\nPOLAR_PASSWORD='+passw+'\nDOWNLOADS_PATH='+dpath+'\nPOLAR_PATH='+ppath)
     return 0
 
-
-SESSIONS = 0
-files = {}
 def FindAndClick(driver,locator,value):
     # Find button
     button = driver.find_element(locator,value)
@@ -44,7 +46,6 @@ def startWeb():
     chrome_options = webdriver.ChromeOptions()
     prefs = {'download.default_directory' : DOWNLOADS_PATH}
     chrome_options.add_experimental_option('prefs', prefs)
-
     s=Service(ChromeDriverManager().install())
     return webdriver.Chrome(service=s,options=chrome_options)
 
@@ -54,82 +55,77 @@ def getLinks(driver):
     driver.set_window_size(300, 300)
     #let page load
     time.sleep(2)
-    #find user and password box
+    #find email and password box
     id_box = driver.find_element(By.ID, "email")
     password_box = driver.find_element(By.ID, "password")
-
+    # fill in email and password
     id_box.send_keys(POLAR_EMAIL)
     password_box.send_keys(POLAR_PASSWORD)
+    #click login
     FindAndClick(driver,By.ID,'login')
-
-    time.sleep(2)
-
+    time.sleep(2) # wait for page to load
     driver.switch_to.new_window('tab')
     driver.get(setURL())
-
-    time.sleep(3)
-
+    time.sleep(2)# wait for page to load
     links = []
     elements = driver.find_elements(By.TAG_NAME,"a")
-    for e in elements:
+    for e in elements: # find every link which includes idetifier min as in minutes
         if 'min' in e.text:
              links.append(e.get_attribute("href"))
     return links 
 
 def exportLinks(driver,links):
-    time.sleep(3)
+    time.sleep(2)# wait for page to load
     print(str(SESSIONS),' training sessions are being downloaded')
     k = 1
     for link in links:
         driver.switch_to.new_window('tab')
         driver.get(link)
         FindAndClick(driver,By.CLASS_NAME,'export')
-        time.sleep(2)
+        time.sleep(2)# wait for page to load
         # Find export buttons
         export_buttons = driver.find_elements(By.CLASS_NAME,'btn.btn-primary.session')
-
-        for i in export_buttons:
+        for i in export_buttons: # iterate through page links till we find Export as CSV link
             if i.text == 'Export as csv':
                 i.click()   
         print('Training session',k,'is being downloaded')
         os.chdir(DOWNLOADS_PATH)
+        #check if file has been downloaded every 2 seconds
         while (True):
             if len(glob.glob(f'{year}{month}10_*_Lacrosse_Rugby.csv'))>0:
                 break
-            time.sleep(2)
+            time.sleep(2) # wait for download to complete
         formatSession(k)
         k+=1
 
 def formatSession(k):
     print('Training session',k,'is being reformated')
-    export_df = pd.read_csv(POLAR_PATH+'Roster.csv',)
+    export_df = pd.read_csv(POLAR_PATH+'Roster.csv') # pull roster for future use
     export_df[['Total distance [m]','Training load score','Muscle load', 'Cardio load']] = 0
     os.chdir(DOWNLOADS_PATH)
-    for file in glob.glob(f'{year}{month}{day}_*_Lacrosse_Rugby.csv'):
-        import_df = pd.read_csv(DOWNLOADS_PATH+file)
-    
-    import_df = import_df[['Player name','Total distance [m]','Training load score','Muscle load', 'Cardio load']]
-    export_df = import_df.append(export_df[~export_df['Player name'].isin(import_df['Player name'].values.tolist())])
-    export_df.sort_values('Player name',inplace=True)
+    for file in glob.glob(f'{year}{month}{day}_*_Lacrosse_Rugby.csv'): # find csv file downloaded for today
+        import_df = pd.read_csv(DOWNLOADS_PATH+file) # import csv file into a dataframe
+        
+    import_df = import_df[['Player name','Total distance [m]','Training load score','Muscle load', 'Cardio load']] #keep only columns neccessary
+    export_df = import_df.append(export_df[~export_df['Player name'].isin(import_df['Player name'].values.tolist())]) #fill in players which are missing data for that session
+    export_df.sort_values('Player name',inplace=True) # sort dataframe by player name 
     export_df.reset_index(drop=True, inplace = True)
-
-    #export file as excel file
     
-    excelfile = f'MLAX-{year}-{month}-{day}-Session-{k}.xlsx'
-    export_df.to_excel(POLAR_PATH+excelfile, index = False)
+    excelfile = f'MLAX-{year}-{month}-{day}-Session-{k}.xlsx' # set excel file name
+    export_df.to_excel(POLAR_PATH+excelfile, index = False) # export formatted dataframe as an excel file
     print('Training session',k,'is being removed from csv files')
-    os.remove(DOWNLOADS_PATH+file) 
+    os.remove(DOWNLOADS_PATH+file)  #remove csv_file
     print('Training session',k,'has been converted to EXCEL')
     files[excelfile] = import_df.shape[0]
 
 def Start():
     driver = startWeb()
-    links = getLinks(driver)
+    links = getLinks(driver) #get links from polar website
     SESSIONS = len(links)
     if(SESSIONS>0):
-        exportLinks(driver,links)
+        exportLinks(driver,links) # iterate through links and pull each sessions data
     else:
         print("No Polar data has not been uploaded today") 
-    driver.quit() 
+    driver.quit() # End Chrome web driver
     print('ENDED SUCCESSFULLY')
     return files
